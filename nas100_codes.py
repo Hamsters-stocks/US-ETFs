01f9550b339fec650a7e81bd64135226a4d50762
Hamsters-stocks <114235291+Hamsters-stocks@users.noreply.github.com>
2022-09-24 01:17:40 +0800

    Update and rename nas100 08-16,18.py to nas100_codes.py

--- nas100_codes.py
+++ nas100_codes.py
@@ -2,9 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+# The following codes is a simplified reference for scapping the page (https://en.wikipedia.org/wiki/Nasdaq-100).
+# It will work for the historical page of 2008 - 2016. 
+# I am still figuring out ways to have a program works for all years. Sorry for the inconvience.
 
 final = pd.DataFrame(data=None)
 while(True):
+    # Enter the dates for the columns name and the url of the historical pages
     date = input('Date:')
     url = input('url:')
 
@@ -22,4 +26,4 @@ while(True):
     result = pd.DataFrame(L,columns = [date])
     final = pd.concat([final,result],axis = 1)
     print(final)
-    final.to_csv('Nasdaq100_08after.csv')
+    final.to_csv('Nasdaq100.csv')
