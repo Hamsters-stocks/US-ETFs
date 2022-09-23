20a5171eeabb6ac2e8e55c51e0f239d91f32d57b
Hamsters-stocks <114235291+Hamsters-stocks@users.noreply.github.com>
2022-09-24 02:35:06 +0800

    Add files via upload

---
+++ S&P500_code.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+SP500 = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#Selected_changes_to_the_list_of_S&P_500_components').text
+soup = BeautifulSoup(SP500,'lxml')
+List = soup.find('table')
+tickers = List.find_all('a', class_ = 'external text')
+final = []
+for ticker in tickers:
+    if ticker.text != 'reports':
+        final.append(ticker.text)
+
+SP500_changes = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#Selected_changes_to_the_list_of_S&P_500_components').text
+soup1 = BeautifulSoup(SP500_changes,'lxml')
+Listraw = soup1.find_all('table')
+List1 = Listraw[1]
+result1 = List1.find_all('td')
+L1 = []
+for x in result1:
+    L1.append(x.text)
+L2 = [L1[i:i+6] for i in range(0,len(L1),6)]
+df = pd.DataFrame(L2)
+df = df.drop([2,4,5], axis=1)
+df.columns = ['date', 'add', 'remove']
+df.set_index('date', inplace = True)
+df.index = pd.to_datetime(df.index)
+df = df.iloc[3:]
+
+print(final)
+print(df)
+sp = pd.DataFrame(data=None)
+for Date, rows in df.iterrows():
+    try:
+        if type(rows[0]) != float:
+            final.remove(rows[0])
+
+        if type(rows[1]) != float:
+            final.append(rows[1])
+        df2 = pd.DataFrame([final], index=[Date])
+        sp = pd.concat([sp, df2], axis=0)
+    except:
+        continue
+
+print(sp)
